7d666b24e5698f3f8c9212c812c4637cb97180ce
9Salmon7 <153941528+9Salmon7@users.noreply.github.com>
2023-12-29 23:54:26 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
-from django.views.generic import View
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class MainView(TemplateView):
+	template_name = "index.html"
+
+class BusinessView(TemplateView):
+	template_name = "Business.html"
+
+class NewsView(TemplateView):
+	template_name = "News.html"
+
+class SustainabilityView(TemplateView):
+	template_name = "Sustainability.html"
+
+class RecruitmentView(TemplateView):
+	template_name = "Recruitment.html"
+
+class ContactView(TemplateView):
+	template_name = "Contact.html"
 
 
 class IndexView(View):
