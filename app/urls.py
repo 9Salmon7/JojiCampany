ddb1ffb933bb630d7091bd5e69cbdf7588041d78
9Salmon7 <153941528+9Salmon7@users.noreply.github.com>
2023-12-29 23:43:54 +0900

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from app import views
+from .views import IndexView, BusinessView, NewsView, SustainabilityView, RecruitmentView, ContactView
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+	path('', IndexView.as_view()),
+	path('business/', BusinessView.as_view()),
+    path('news/', NewsView.as_view()),
+    path('Sustainability/', SustainabilityView.as_view()),
+    path('Recruitment/', RecruitmentView.as_view()),
+    path('Contact/', ContactView.as_view()),
 ]
