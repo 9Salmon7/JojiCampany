c7df17291e43273a7913ffdbc9a2ca53e1658511
9Salmon7 <153941528+9Salmon7@users.noreply.github.com>
2023-12-29 23:44:20 +0900

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,8 @@ from .views import IndexView, BusinessView, NewsView, SustainabilityView, Recrui
 urlpatterns = [
 	path('', IndexView.as_view()),
 	path('business/', BusinessView.as_view()),
-    path('news/', NewsView.as_view()),
-    path('Sustainability/', SustainabilityView.as_view()),
-    path('Recruitment/', RecruitmentView.as_view()),
-    path('Contact/', ContactView.as_view()),
+    	path('news/', NewsView.as_view()),
+    	path('Sustainability/', SustainabilityView.as_view()),
+    	path('Recruitment/', RecruitmentView.as_view()),
+    	path('Contact/', ContactView.as_view()),
 ]
