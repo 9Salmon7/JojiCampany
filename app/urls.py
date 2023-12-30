76a300a1e1287bc6cf6d871324b2e3f1c7e35787
9Salmon7 <153941528+9Salmon7@users.noreply.github.com>
2023-12-30 18:20:41 +0900

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from .views import MainView, BusinessView, NewsView, SustainabilityView, RecruitmentView, ContactView
 
 urlpatterns = [
-	path("", views.IndexView.as_view(), name="index"),
+	path("", MainView.as_view()),
 	path('business/', BusinessView.as_view()),
     	path('news/', NewsView.as_view()),
     	path('Sustainability/', SustainabilityView.as_view()),
