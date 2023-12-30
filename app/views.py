ade2443be9cced90adc10c5bc7e75f4c794c287f
9Salmon7 <153941528+9Salmon7@users.noreply.github.com>
2023-12-30 18:21:35 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,24 +3,19 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class MainView(TemplateView):
-	template_name = "index.html"
+	template_name = "app/index.html"
 
 class BusinessView(TemplateView):
-	template_name = "Business.html"
+	template_name = "app/Business.html"
 
 class NewsView(TemplateView):
-	template_name = "News.html"
+	template_name = "app/News.html"
 
 class SustainabilityView(TemplateView):
-	template_name = "Sustainability.html"
+	template_name = "app/Sustainability.html"
 
 class RecruitmentView(TemplateView):
-	template_name = "Recruitment.html"
+	template_name = "app/Recruitment.html"
 
 class ContactView(TemplateView):
-	template_name = "Contact.html"
-
-
-class IndexView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        return render(request, "app/index.html")
+	template_name = "app/Contact.html"
